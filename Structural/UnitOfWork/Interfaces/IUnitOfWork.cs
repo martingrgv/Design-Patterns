f24a8b40d8f8adcdd11af35fb03344656454cba5
Martin Georgiev <MDGeorgiev@tbibank.bg>
2025-01-15 12:45:32 +0200

    Added unit of work pattern

---
+++ Structural/UnitOfWork/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace UnitOfWork.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        public void Add<TEntity>(TEntity entity) where TEntity : IEntity;
+        public void AddRange<TEntity>(List<TEntity> entities) where TEntity : IEntity;
+        public Task FindAsync<TEntity>(int id) where TEntity : IEntity;
+        public void Remove<TEntity>(TEntity entity) where TEntity : IEntity;
+        public Task SaveChangesAsync();
+    }
+}
---
+++ Structural/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using UnitOfWork.Interfaces;
+
+namespace UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private List<TEntity> GetSet<TEntity>() where TEntity : IEntity => new();
+
+        public void Add<TEntity>(TEntity entity) where TEntity : IEntity
+        {
+            GetSet<TEntity>().Add(entity);
+        }
+
+        public void AddRange<TEntity>(List<TEntity> entities) where TEntity : IEntity
+        {
+            GetSet<TEntity>().AddRange(entities);
+        }
+
+        public Task FindAsync<TEntity>(int id)  where TEntity : IEntity
+        {
+            return Task.FromResult(GetSet<TEntity>().FirstOrDefault(e => e.Id == id));
+        }
+
+        public void Remove<TEntity>(TEntity entity) where TEntity : IEntity
+        {
+            GetSet<TEntity>().Remove(entity);
+        }
+
+        public Task SaveChangesAsync()
+        {
+            Console.WriteLine("Saved changes");
+
+            return Task.CompletedTask;
+        }
+    }
+}
